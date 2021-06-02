871df0ee42707e10cfbb9cf675d5071dd039525e
Shubham19102007 <76561542+Shubham19102007@users.noreply.github.com>
2021-06-02 11:16:45 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,28 @@
+class launcher{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB= point
+        this.launcherObj = Constraint.create(options);
+        World.add(world, this.launcherObj);
+    }
+
+         fly(){
+        this.launcher.bodyA = null
+    }
+
+
+    display(){
+        if (this.launcherObj.bodyA){
+        var pointA = this.launcherObj.bodyA.position;
+        var pointB = this.pointB
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
